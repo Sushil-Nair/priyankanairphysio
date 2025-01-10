a8a93298470f31131da3d4dc0840e1c43c924a9d
Sushil-Nair <nair.sushil621@gmail.com>
2025-01-11 00:53:32 +0530

    app.jsx resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Booking from './pages/Booking';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Booking from './Pages/Booking';
 
 function App() {
   return (
